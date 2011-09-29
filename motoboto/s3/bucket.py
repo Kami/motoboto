ea8c08c811ae2e77de7102aefb8e6d153668bde5
Doug Fort <dougfort@dougfort.com>
2011-09-29 17:01:31 -0400

    add get_space_used to bucket
    This isn't in s3, but it's useful for our benchmark validity check.

--- motoboto/s3/bucket.py
+++ motoboto/s3/bucket.py
@@ -7,7 +7,9 @@ simulate a boto Bucket object
 import json
 import logging
 
-from lumberyard.http_util import compute_collection_hostname, compute_uri
+from lumberyard.http_util import compute_default_hostname, \
+        compute_collection_hostname, \
+        compute_uri
 from lumberyard.http_connection import HTTPConnection
 
 from motoboto.s3.key import Key
@@ -54,4 +56,32 @@ class Bucket(object):
             self._config.auth_key,
             self._config.auth_key_id
         )
+
+    def get_space_used(self):
+        """
+        get disk space statistics for this bucket
+        """
+
+        http_connection = HTTPConnection(
+            compute_default_hostname(),
+            self._config.user_name,
+            self._config.auth_key,
+            self._config.auth_key_id
+        )
+        method = "GET"
+        uri = compute_uri(
+            "/".join([
+                "customers", 
+                self._config.user_name, 
+                "collections",
+                self._collection_name
+            ]),
+            action="space_usage"
+        )
+
+        response = http_connection.request(method, uri)
+        data = response.read()
+        http_connection.close()
+    
+        return json.loads(data)
 
